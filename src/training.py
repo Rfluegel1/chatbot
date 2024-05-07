21f779be85e96a995be150a8f1780a44404a1603
reidfluegel <reid.fluegel@gmail.com>
2024-05-07 10:36:47 -0500

    correct tensorflow paths

--- src/training.py
+++ src/training.py
@@ -5,9 +5,10 @@ import numpy
 import nltk
 
 from nltk.stem import WordNetLemmatizer
-# from tensorflow.keras.models import sequential
-# from tensorflow.keras.layers import Dense, Activation, Dropout
-# from tensorflow.keras.optimizers import SGD
+from tensorflow.python.keras.models import sequential
+from tensorflow.python.keras.models import sequential
+from tensorflow.python.keras.layers import Dense, Activation, Dropout
+from tensorflow.python.keras.optimizer_v2 import gradient_descent as GSD
 
 
 def add(a, b):
